4c23167b2953f0fe508956652d82b16952842403
Harry Turnbull <hturnbull@live.co.uk>
2020-03-18 17:59:07 +0000

    added capacity inst var and error for if dock is full

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,14 +1,20 @@
 class DockingStation
+  def initialize
+    @capacity = 20
+  end
+
   def release_bike
     fail "No bikes available." unless @bike
     @bike
   end
 
   def dock_bike(bike)
+    fail "Dock full." if @bikes_docked == 1
+    @bikes_docked = 1
     @bike = bike
   end
 
-  attr_reader :bike
+  attr_reader :bike, :capacity
 end
 
 class Bike
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -7,6 +7,9 @@ describe DockingStation do
     expect(station).to respond_to(:release_bike)
   end
 
+  it "has a default capacity of 20 bikes" do
+    expect(station.capacity).to eq 20
+  end
 
   describe "#release_bike" do
     it "returns error if no bike available" do
@@ -19,18 +22,28 @@ describe DockingStation do
       expect(station.release_bike).to be_instance_of(Bike)
     end
   end
+  
+  describe "#dock_bike" do
+    it "responds with 1 argument" do
+      expect(station).to respond_to(:dock_bike).with(1).argument
+    end
+    
+    it "returns error if already has bike docked" do
+      bike = Bike.new
+      another_station = DockingStation.new
+      another_station.dock_bike(bike)
+      another_bike = Bike.new
+      expect {another_station.dock_bike(another_bike)}.to raise_error "Dock full."
+    end
 
-  it "responds to #dock_bike with 1 argument" do
-    expect(station).to respond_to(:dock_bike).with(1).argument
+    it "docks the passed bike" do
+      bike = Bike.new
+      expect(station.dock_bike(bike)).to eq bike
+    end
   end
 
   it "responds to #bike" do
     expect(station).to respond_to(:bike)
-  end
-
-  it "#docks_bike docks the passed bike" do
-    bike = Bike.new
-    expect(station.dock_bike(bike)).to eq bike
   end
 
   it "returns the docked bike" do
