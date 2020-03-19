9ee5d346a4c68be1b9feb56a038938cc83e0bd50
Harry Turnbull <hturnbull@live.co.uk>
2020-03-19 18:43:24 +0000

    abstracted full? and empty? from dock_bike and release_bike and made private

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -4,16 +4,27 @@ class DockingStation
   end
 
   def release_bike
-    fail "No bikes available." if @storage.empty?
+    fail "No bikes available." if empty?
     @storage.pop
   end
 
   def dock_bike(bike)
-    fail "Dock full." if @storage.size == 20
+    fail "Dock full." if full?
     @storage << bike
   end
 
   attr_reader :storage
+
+  private
+  
+  def full?
+    @storage.size >= 20
+  end
+  
+  def empty?
+    @storage.empty?
+  end
+
 end
 
 class Bike
